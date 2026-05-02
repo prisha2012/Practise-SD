d86c7e25b5fe33f4a86b10b5ba6db7c82104f60b
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-05-02 17:22:36 +0530

    new

--- P1/add.js
+++ P1/add.js
@@ -1,14 +1,23 @@
-document.getElementById("addBtn").addEventListener("click", addItem);
+const input=document.getElementById("input");
+const preview=document.getElementById("preview");
 
+input.addEventListener("input",()=>{
+    preview.innerText=input.value?"Typing: "+input.value: "";
+});
+input.addEventListener("keypress",(e)=>{
+    if(e.key==="Enter"){
+        addItem();
+    }
+});
 function addItem() {
-    const input=document.getElementById("input");
     const value=input.value.trim();
     if(!value)return;
-
+ 
     let items=JSON.parse(localStorage.getItem("items"))||[];
     items.push(value);
 
-    localStorage.setItem("items", JSON.stringify(items));
+    localStorage.setItem("items",JSON.stringify(items));
     input.value="";
+    preview.innerText="";
     alert("Item added successfully!");
 }
--- P1/list.js
+++ P1/list.js
@@ -1,3 +1,4 @@
+let selectedIndex = -1; 
 function loadItems(){
     const list=document.getElementById("list");
     list.innerHTML="";
@@ -5,36 +6,47 @@ function loadItems(){
     items.forEach((item,index)=>{
         const li=document.createElement("li");
         li.className="flex justify-between bg-zinc-700 p-2 rounded";
+         li.innerText=item;
 
-        const span=document.createElement("span");
-        span.innerText=item;
-
-        const div=document.createElement("div");
-
-        const editBtn=document.createElement("button");
-        editBtn.innerText="Edit";
-        editBtn.onclick=()=>{
-            const newValue=prompt("Edit item:", item);
-            if(newValue&&newValue.trim()!=""){
-                items[index]=newValue;
-                localStorage.setItem("items", JSON.stringify(items));
-                loadItems();
-            }
-        };
-        const deleteBtn=document.createElement("button");
-        deleteBtn.innerText="Delete";
-        deleteBtn.onclick=()=>{
-            items.splice(index,1);
-            localStorage.setItem("items", JSON.stringify(items));
-            loadItems();
-        };
-        div.appendChild(editBtn);
-        div.appendChild(deleteBtn);
-        li.appendChild(span);
-        li.appendChild(div);
+        li.addEventListener("click",()=>{
+            selectedIndex=index;
+            document.querySelectorAll("li").forEach((el)=>{
+                el.classList.remove("bg-blue-500");
+            });
+            
+            li.classList.add("bg-blue-500");
+            });
+        //     const confirmDelete=confirm("Do you want to delete this item?");
+        //     if(!confirmDelete)return;
+        //     items.splice(index,1);
+        //     localStorage.setItem("items", JSON.stringify(items));
+        //     loadItems();
+           
+        // });
+        li.addEventListener("dblclick",(e)=>{
+            e.stopPropagation(); 
+                const newValue=prompt("Edit item:", item);
+                if(newValue&&newValue.trim()!==""){
+                    items[index]=newValue;
+                    localStorage.setItem("items", JSON.stringify(items));
+                    loadItems();
+                }
+        });
         list.appendChild(li);
 
     });
+        document.onkeydown=function(e){
+            if(e.key==="Delete" && selectedIndex!==-1){
+                let confirmDelete=confirm("Do you want to delete this item?");
+                if(!confirmDelete)return;
+                items.splice(selectedIndex,1);
+                localStorage.setItem("items", JSON.stringify(items));
+                selectedIndex=-1;
+                loadItems();
+            }
+        }
+    
+        
 }
 
 loadItems();
